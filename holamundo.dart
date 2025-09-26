e82f95e875016b68787f22d88b2a59da35b77206
Odalis Soto <202501564@est.umss.edu>
2025-09-26 16:23:36 -0400

    primer commit

---
+++ holamundo.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+void main (){
+  //String nombre = "Odalis"; 
+  //int edad = 18;
+  //double estatura = 1.58;
+  //bool licenciaConducir= false; 
+  // para pedir por consola
+  print ("Ingresa tu nombre");
+  String? nombre1 = stdin.readLineSync();
+  //String? nombre2 = stdin.readLineSync()!;  
+  print("Tu nombre es $nombre1"); 
+  //print("Tu nombre es" + nombre);
+  //print ("Tu edad es $edad");
+  //print("tu estatura es $estatura");
+  //print("Usted puede manejar $licenciaConducir");
+}

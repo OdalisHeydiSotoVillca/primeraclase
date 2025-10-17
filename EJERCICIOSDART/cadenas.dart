f2bcf0c9051470439bb9ecab46d92ad8562e3a55
Odalis Soto <202501564@est.umss.edu>
2025-10-17 16:40:46 -0400

    Clase2 cadenas

---
+++ EJERCICIOSDART/cadenas.dart
@@ -0,0 +1,10 @@
+void main(){
+  String name = "ODALIS";
+  print(name);
+  print (name.toLowerCase());
+  print(name.length);
+  print(name.toLowerCase().contains("a"));
+  print (name.trim());
+  print(name.contains("a"));
+
+}
